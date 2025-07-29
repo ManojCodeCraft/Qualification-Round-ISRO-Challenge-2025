07c8e872ed220e68925ddd8d9bf94964296b1f64
Manoj Kumar <162701324+ManojCodeCraft@users.noreply.github.com>
2025-07-29 12:02:48 +0530

    Motor_Arming.py

--- QUALIFICATION ROUND/tests/Motor_Arming.py
+++ QUALIFICATION ROUND/tests/Motor_Arming.py
@@ -1,12 +1,10 @@
 from pymavlink import mavutil
 import time
 
-# Connect to the Cube+
 master = mavutil.mavlink_connection('/dev/ttyUSB0', baud=57600)
 master.wait_heartbeat()
 print("✅ Heartbeat received!")
 
-# Arm the motors
 print("Arming motors...")
 master.mav.command_long_send(
     master.target_system, master.target_component,
@@ -14,10 +12,10 @@ master.mav.command_long_send(
     0, 1, 0, 0, 0, 0, 0, 0
 )
 
-# Wait for 10 seconds while motors are armed
+
 time.sleep(10)
 
-# Disarm the motors
+
 print("Disarming motors...")
 master.mav.command_long_send(
     master.target_system, master.target_component,
