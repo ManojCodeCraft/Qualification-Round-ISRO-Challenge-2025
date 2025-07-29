61f7cb495f985114d3896a9687a47541b4c73b4f
Manoj Kumar <162701324+ManojCodeCraft@users.noreply.github.com>
2025-07-29 12:06:47 +0530

    TFmini_Lidar_To_Raspberrypi.py

--- QUALIFICATION ROUND/tests/TFmini_Lidar_To_Raspberrypi.py
+++ QUALIFICATION ROUND/tests/TFmini_Lidar_To_Raspberrypi.py
@@ -1,12 +1,12 @@
 import serial
 import time
-# Open serial connection for TFmini-S
+
 ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=1)
 while True:
-    count = ser.in_waiting  # Check if data is available
+    count = ser.in_waiting  
     if count > 8:
-        recv = ser.read(9)  # Read 9 bytes
-        if recv[0] == 0x59 and recv[1] == 0x59:  # Check header bytes
-            distance = recv[2] + recv[3] * 256  # Calculate distance
+        recv = ser.read(9)  
+        if recv[0] == 0x59 and recv[1] == 0x59:  
+            distance = recv[2] + recv[3] * 256  
             print(f"Distance: {distance} cm")
     time.sleep(0.01)
